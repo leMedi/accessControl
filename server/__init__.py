79245477a3f44b3503ce9855b5cbf69d4167a229
mehdi-elhaij <mehdi.elhaij@hotmail.com>
2018-02-05 16:35:01 +0000

    add employee

--- server/__init__.py
+++ server/__init__.py
@@ -1,12 +1,17 @@
 # Import flask and template operators
 from flask import Flask, render_template, request
+from flask_mongoengine import MongoEngine
 from wtforms import Form, BooleanField, StringField, PasswordField, validators
+import datetime
 
 # Define the WSGI application object
 app = Flask(__name__)
 
 # Configurations
 app.config.from_object('config')
+
+# Database Connection
+db = MongoEngine(app)
 
 # Sample HTTP error handling
 @app.errorhandler(404)
@@ -22,13 +27,23 @@ def index():
 ##############
 ## Employees
 #########
+
+## ** Employee Class ** ##
+class Employee(db.Document):
+    first_name = db.StringField(required=True, max_length=200)
+    last_name = db.StringField(required=True, max_length=200)
+    department = db.StringField(required=True, max_length=200)
+    code = db.StringField(required=True, max_length=20)
+    date_creation = db.DateTimeField(default=datetime.datetime.utcnow)
+ 
+
 ## ** forms ** ##
-class newEmployeeForm(Form):
+# TODO: use model_form instead
+class EmployeeForm(Form):
     first_name = StringField('First Name', [validators.Length(min=4, max=25)])
     last_name = StringField('Last Name', [validators.Length(min=4, max=25)])
-    departement = StringField('Departement', [validators.Length(min=4, max=25)])
+    department = StringField('Department', [validators.Length(min=4, max=25)])
     code = StringField('Code', [validators.Length(min=5, max=35)])
-
 
 ## ** routes ** ##
 
@@ -40,12 +55,17 @@ def list_employee():
 # Add new employee
 @app.route('/employee/add', methods=['GET', 'POST'])
 def add_employee():
-    form = newEmployeeForm(request.form)
+    form = EmployeeForm(request.form)
     if request.method == 'POST' and form.validate():
-        return "hi"
+        e = Employee(
+            first_name=form.first_name.data,
+            last_name=form.last_name.data,
+            department=form.department.data,
+            code=form.code.data
+        )
+        e.save()
+        return "ok"
     return render_template('employees/add.html', form=form)
-
-
 
 ##############
 ## End Employees
